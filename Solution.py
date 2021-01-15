fc87818eb39ee6f3b645452efaf97133c08da23a
Seamus-Grady <?`4$d4^^XAJgD"sW>
2021-01-15 12:19:53 -0600

    Changed the inital loop to build the tree to handle problems without parenthesis implementing parenthesis next

--- Solution.py
+++ Solution.py
@@ -16,23 +16,25 @@ def buildTheTree(line):
     lineWithoutSpaces = line.split()
     currentProblem = mathTree()
     previousNode = node()
-    for i in range(0, len(lineWithoutSpaces)-1):
+    for i in range(0, len(lineWithoutSpaces)):
         currentNode = node()
         if i == 0:
             currentNode.value = lineWithoutSpaces[i]
             currentProblem.root = currentNode
             previousNode = currentNode
             left = 1
-        if left == 1:
+        elif left == 1:
             currentNode.value = lineWithoutSpaces[i]
             currentProblem.root = currentNode
             currentProblem.root.left = previousNode
             previousNode = currentNode
             left = 0
             right = 1
-        if right == 1:
+        elif right == 1:
             currentNode.value = lineWithoutSpaces[i]
             currentProblem.root.right = currentNode
-            previousNode = currentNode
             left = 1
             right = 0
+
+for line in sys.stdin:
+    buildTheTree(line.rstrip())
